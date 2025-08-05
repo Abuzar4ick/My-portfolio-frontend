32d5f572c157fd50304ed987842b3a8c4de8e3b9
Abuzar <abuzarrrakhmonov@gmail.com>
2025-08-05 14:22:54 +0500

    Particles deleted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,11 @@
 import Info from "../components/Info"
 import SectionToggle from "../components/SectionToggle"
-// particles
-import ParticlesBg from 'particles-bg'
 
 const Home = () => {
   return (
     <>
       <Info />
       <SectionToggle />
-      <ParticlesBg type="cobweb" bg={true} color="#e5bc29" /> 
     </>
   )
 }
