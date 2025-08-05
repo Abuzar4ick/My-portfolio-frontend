ebc7068416c9c725b0d5844bb0d961869ad264c5
Abuzar <abuzarrrakhmonov@gmail.com>
2025-08-05 14:14:23 +0500

    Particles added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Info from "../components/Info"
 import SectionToggle from "../components/SectionToggle"
+// particles
+import ParticlesBg from 'particles-bg'
 
 const Home = () => {
   return (
     <>
       <Info />
       <SectionToggle />
+      <ParticlesBg type="cobweb" bg={true} color="#e5bc29" /> 
     </>
   )
 }
